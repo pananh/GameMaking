8084c1187da9578750acbb014a70ea91999549f4
pananh <pananh@gmail.com>
2025-03-14 16:04:15 +0700

    cq 25.03.14 16.04

---
+++ InterestPerMonth.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeGym
+{
+    public class CodeGymExercise_InterestRate
+    {
+        public static void Main(string[] args)
+        {
+            double money;
+            uint month;
+            double interestRate;
+
+            money = Get_Double("Enter the amount of money: ");
+            month = Get_UInt("Enter the number of months: ");
+            interestRate = Get_UDouble("Enter the interest rate per year: ");
+
+            for (int i = 0; i < month; i++)
+            {
+                // period = 1 month
+                money += money * interestRate / 100 / 12;
+            }
+            Console.WriteLine("The amount of money after " + month + " months is: " + money);
+        }
+
+        static double Get_Double(string showString)
+        {
+            double number;
+            bool isNumber;
+            Console.Write(showString);
+            do
+            {
+                isNumber = double.TryParse(Console.ReadLine(), out number);
+                if (!isNumber)
+                {
+                    Console.Write("Invalid input. Please enter a number again. " + showString);
+                }
+            } while (!isNumber);
+            return number;
+        }
+
+        static double Get_UDouble(string showString)
+        {
+            double number;
+            bool isNumber;
+            Console.Write(showString);
+            do
+            {
+                isNumber = double.TryParse(Console.ReadLine(), out number);
+                if (!isNumber || number < 0)
+                {
+                    Console.Write("Invalid input. Please enter a number again. " + showString);
+                }
+            } while (!isNumber || number < 0);
+            return number;
+        }
+
+        static int Get_Int(string showString)
+        {
+            int number;
+            bool isNumber;
+            Console.Write(showString);
+            do
+            {
+
+                isNumber = int.TryParse(Console.ReadLine(), out number);
+                if (!isNumber)
+                {
+                    Console.Write("Invalid input. Please enter a number again. " + showString);
+                }
+            } while (!isNumber);
+            return number;
+        }
+        static uint Get_UInt(string showString)
+        {
+            uint number;
+            bool isNumber;
+            Console.Write(showString);
+            do
+            {
+                isNumber = uint.TryParse(Console.ReadLine(), out number);
+                if (!isNumber)
+                {
+                    Console.Write("Invalid input. Please enter a number again. " + showString);
+                }
+            } while (!isNumber);
+            return number;
+        }
+    }
+}
+
