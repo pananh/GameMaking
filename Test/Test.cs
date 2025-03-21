8ea8c673bbecb8feb8239caab44360b4b5eff149
pananh <pananh@gmail.com>
2025-03-21 17:48:41 +0700

    cq 25.03.21 17.48

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Linq;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -23,11 +25,74 @@ namespace ListExample
 
         }
 
+        public enum Color
+        {
+            Red = 0,
+            Blue = 1,
+            Green = 2,
+            Yellow = 3,
+            White = 4,
+        }
+
+        static Color ReturnColor(int colorIndex)
+        {
+            switch (colorIndex)
+            {
+                case 0: return Color.Red;
+                case 1: return Color.Green;
+                case 2: return Color.Yellow;
+                case 3: return Color.White;
+                case 4: return Color.White;
+                default: return 0;
+            }
+        }
+
+        
+
+        public class House
+        {
+
+            public string name;
+            public int numberPerson;
+            public string addhouse;
+            public Color color;
+            
+            public House(string namex, int numberPersonx, string addhousex, Color colorx)
+            {
+                //this.name = namex;
+                //this.number = numberx;
+                //this.addhouse = addhousex;
+                //this.color = colorx;
+                name = namex;
+                numberPerson = numberPersonx;
+                addhouse = addhousex;
+                color = colorx;
+            }
+
+        }
+
+        static House RandomHouse()
+        {
+            Random randomVar = new Random ();
+            string nameh = "";
+            for (int k = 0; k < 5; k++)
+            {
+                nameh += Convert.ToChar(randomVar.Next(97, 122));
+            }
+            string addh = "";
+            for (int k = 0; k < 10; k++)
+            {
+                addh += Convert.ToChar(randomVar.Next(97, 122));
+            }
+            House returnHouse = new House(nameh, randomVar.Next(1, 10), addh, ReturnColor(randomVar.Next(0, 4)));
+
+            return returnHouse;
+        }
 
         public static void Main(string[] args)
         {
             Random randomVar = new Random();
-            int n = 6;
+            int n = 15;
 
             string[] Color = new string[] { "Red", "Green", "Yellow", "Blue", "Black", "White" };
             string[] Country = new string[] { "Vietnam", "USA", "Japan", "Korean", "China", "EU" };
@@ -46,8 +111,7 @@ namespace ListExample
                     Origin = Country[randomVar.Next(0, 5)],
                 };
             }
-
-            PrintArray("Name Color Price Origin - Array Mobile Origin", arrayMobile);
+            //PrintArray("Name Color Price Origin - Array Mobile Origin", arrayMobile);
 
             // list
             List <Mobile> listMobile = new List<Mobile>();
@@ -66,7 +130,7 @@ namespace ListExample
             }
             var hoso = queueMobile.Dequeue();
             hoso = queueMobile.Dequeue();
-            PrintQueue("Name Color Price Origin - Queue Mobile after taken 2",queueMobile);
+            //PrintQueue("Name Color Price Origin - Queue Mobile after taken 2",queueMobile);
 
             //Stack
             Stack <Mobile> stackMobile = new Stack<Mobile>();
@@ -76,13 +140,73 @@ namespace ListExample
             }
             hoso = stackMobile.Pop();
             hoso = stackMobile.Pop();
-            PrintStack("Name Color Price Origin - Stack Mobile after taken 2", stackMobile);
+            //PrintStack("Name Color Price Origin - Stack Mobile after taken 2", stackMobile);
 
 
             //Linked List
+            LinkedList<Mobile> linkedListMobile = new LinkedList<Mobile>();
+            
+            var node0 = linkedListMobile.AddFirst(arrayMobile[0]);
+            var node1 = linkedListMobile.AddLast(arrayMobile[1]);
+            var node2 = linkedListMobile.AddAfter(node0, arrayMobile[2]);
+            LinkedListNode <Mobile> node3 = linkedListMobile.AddLast(arrayMobile[3]);
+            node0.Value = arrayMobile[4];
+            //Console.WriteLine("Node0 test " + node0.Value.Name + " " + node0.Value.Origin + " " + node0.Value.Price);
+            //PrintLinkedList("Name Color Price Origin - Linked Node",linkedListMobile);
+
+            // Dictionary
+            Console.WriteLine("Dictionary");
+
+            List <House> listHouseMyDinh = new List<House>();
+
+            // Tao ngau nhien n = 10 nha
+
+            for (int i = 0; i < n; i++)
+            {
+                listHouseMyDinh.Add(RandomHouse());
+            }
+
+            // In List House My Dinh
+            foreach (var i in listHouseMyDinh)
+            {
+                Console.WriteLine(i.name + " " + i.addhouse + " " + i.numberPerson + " " + i.color);
+            }
+
+            // Dictionary by Color
+            Dictionary<Color, List<House>> dictionaryHouseColor = new Dictionary<Color, List<House>> ();
+            // Them vao Dictionary
+            foreach (var i in listHouseMyDinh)
+            {
+               Color colorAdd = i.color;
+               int numberHouse = i.numberPerson;
+                if (dictionaryHouseColor.ContainsKey(colorAdd))
+                {
+                    dictionaryHouseColor[colorAdd].Add(i);
+                }
+                else
+                {
+                    dictionaryHouseColor.Add(colorAdd, new List<House>());
+                }
+
+            }
+
+            Console.WriteLine("Dictionary by Color");
+            foreach (var i in dictionaryHouseColor)
+            {
+                Console.WriteLine(i.Key + ": ");
+                    
+                    foreach (var j in i.Value)
+                {
+                    Console.WriteLine(j.name + " " + j.addhouse + " " + j.numberPerson + " " + j.color);
+                }
+                    
+            }
+
+            // Dictionary by number
+            
 
 
-
+            Console.WriteLine("----------");
 
             //change
             for (int i = 0; i < arrayMobile.Length; i++)
@@ -95,8 +219,8 @@ namespace ListExample
                     Origin = Country[randomVar.Next(0, 5)],
                 };
             }
-            PrintArray("Array change",arrayMobile);
-            PrintList("List check", listMobile);
+            //PrintArray("Array change",arrayMobile);
+            //PrintList("List check", listMobile);
 
             // find in Array
             Mobile find1 = listMobile[0];
@@ -108,17 +232,29 @@ namespace ListExample
                     found1 = i ; break;
                 }
             }
-            Console.WriteLine("Origin: listMobile[0] = arrayMobile[{0}]", found1);
+            //Console.WriteLine("Origin: listMobile[0] = arrayMobile[{0}]", found1);
 
             
             // find in List
             Mobile find2 = arrayMobile[0];
             var found2 = listMobile.FindIndex(x => x.Origin == find2.Origin);
-            Console.WriteLine("Origin:  arrayMobile[0] = listMobile[{0}]",found2);
+            //Console.WriteLine("Origin:  arrayMobile[0] = listMobile[{0}]",found2);
 
 
 
 
+        }
+
+      
+        static void PrintLinkedList(string x, LinkedList<Mobile> list)
+        {
+            Console.WriteLine(x);
+            int i = 0;
+            foreach (var item in list)
+            {
+                Console.WriteLine(i + ". " + item.Name + " " + item.Color + " " + item.Price + " " + item.Origin);
+                i++;
+            }
         }
 
         static void PrintStack(string x, Stack<Mobile> stackMobile)
