d00320155527e0c20eac1dffd90f8dc335032846
pananh <pananh@gmail.com>
2025-03-31 12:36:46 +0700

    cq 25.03.31 12.36

---
+++ Circle Cylinder Class.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGym
+{
+	public class Circle
+	{
+		const double PI = Math.PI;
+		protected double radius;
+		protected string color;
+
+		public double Radius
+		{
+			get { return radius; }
+			set { radius = value; }
+		}
+
+		public string Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+
+		public double Area()
+		{
+			return radius * radius * PI;
+		}
+
+		public override string ToString()
+		{
+			return "Here is Circle class";
+		}
+
+	}
+	class Cylinder : Circle
+	{
+		public double height { get; set; }
+
+		public double Volumne()
+		{
+			return this.Area() * height;
+		}
+
+		public override string ToString()
+		{
+			{
+				return "Here is Cylinder class";
+			}
+
+		}
+	}
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Circle c = new Circle();
+			c.Radius = 10;
+			c.Color = "Green";
+			Console.WriteLine(c.ToString());
+			Console.WriteLine("Radius {0:F2}", c.Radius);
+			Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
+
+
+			Cylinder cylinder = new Cylinder();
+			cylinder.Radius = c.Radius;
+			cylinder.Color = c.Color;
+			cylinder.height = 20;
+
+
+			Console.WriteLine("------------");
+			c.Radius = 100; c.Color = "Red";        // Test thu doi gia tri, nhung Cylinder khong thay doi
+			Console.WriteLine(c.ToString());
+			Console.WriteLine("Radius {0:F2}", c.Radius);
+			Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
+
+			Console.WriteLine("------------");
+			Console.WriteLine(cylinder.ToString());
+			Console.WriteLine("Cylinder radius {0:F2} - height {1:F2}", cylinder.Radius, cylinder.height);
+			Console.WriteLine("Color is " + cylinder.Color);
+			Console.WriteLine("Volumne is {0:F2}", cylinder.Volumne());
+
+			Console.WriteLine("------------");
+			cylinder.Radius = 200;
+			cylinder.Color = "White";
+			cylinder.height = 900;
+			Console.WriteLine(c.ToString());
+			Console.WriteLine("Radius {0:F2}", c.Radius);
+			Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
+
+		}
+	}
+
+
+}
+
+
+
--- CodeGymBt/CodeGym.cs
+++ CodeGymBt/CodeGym.cs
@@ -2,212 +2,107 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeGame
+namespace CodeGym
 {
-    // Symbol to Show: Snake, Food, Wall, Empty, Border
-    public class Symbol
+    public class Circle
     {
-        public char Snake { get; set; }     
-        public char Food { get; set; }
-        public char Wall { get; set; }
-        public char Empty { get; set; }
-        public char Border { get; set; }
+        const double PI = Math.PI;
+        protected double radius;
+        protected string color;
 
-        public Symbol() 
+        public double Radius
         {
-            Snake = 'O';                Food = 'F';
-            Wall = '#';                 Empty = ' ';
-            Border = '*';
+            get { return radius; }
+            set { radius = value; }
         }
-        public Symbol(char snake, char food, char wall, char empty, char border)
-        {
-            Snake = snake; Food = food;
-            Wall = wall; Empty = empty;
-            Border = border;
-        }
-    };
 
-    // Land status
-    public class LandStt
-    {
-        public bool IsEmpty { get; set; }
-        public bool IsSnake { get; set; }
-        public bool IsWall { get; set; }
-        public bool IsFood { get; set; }
-        public bool IsBorder { get; set; }
-        public LandStt()
+        public string Color
         {
-            IsEmpty = true; IsBorder = false;
-            IsSnake = false; IsWall = false; IsFood = false;
+            get { return color; }
+            set { color = value; }
         }
-        public void SetBorder() { IsBorder = true; IsEmpty = false; IsSnake = false; IsWall = false; IsFood = false; }
-        public void SetEmpty() { IsEmpty = false; IsSnake = false; IsWall = false; IsFood = false; }
-        public void SetSnake() { IsSnake = true; IsEmpty = false; IsWall = false; IsFood = false; }
-        public void SetWall() { IsWall = true; IsEmpty = false; IsSnake = false; IsFood = false; }
-        public void SetFood() { IsFood = true; IsEmpty = false; IsSnake = false; IsWall = false; }
-        public void AddFood() { IsFood = true; IsEmpty = false; }
-        public void RemoveFood() { IsFood = false; IsEmpty = true; }
+
+        public double Area()
+        {
+            return radius * radius * PI;
+        }
+
+        public override string ToString()
+        {
+            return "Here is Circle class";
+        }
+
     }
-
-    // Position
-    public class Pos
+    class Cylinder : Circle
     {
-        public int x;           public int y;
-        public int xMin { get; private set; }           public int xMax { get; private set; }
-        public int yMin { get; private set; }           public int yMax { get; private set; }
+        public double height { get; set; }
 
-        public Pos()
+        public double Volumne()
         {
-            x = 0; y = 0;
-            xMin = 0; yMin = 0;
-            xMax = 21; yMax = 21;
+            return this.Area() * height;
         }
 
-        public Pos(int x, int y)
+        public override string ToString()
         {
-            this.x = x; this.y = y;
+            {
+                return "Here is Cylinder class";
+            }
+
         }
-
-        public Pos(int x, int y, int xMin, int xMax, int yMin, int yMax)
-        {
-            this.x = x; this.y = y;
-            this.xMin = xMin;  this.xMax = xMax; this.yMin = yMin; this.yMax = yMax;
-        }
-
-        public void PosSnake(int x, int y)
-        {
-            if (x < xMin ) x = xMin;            if (x > xMax) x = xMax;
-            if (y < yMin) y = yMin;             if (y > yMax) y = yMax;
-
-            this.x = x;            this.y = y;
-        }
-
-        public void SetXYMinMax(int xMin, int xMax, int yMin, int yMax)
-        {
-            this.xMin = xMin;
-            this.xMax = xMax;
-            this.yMin = yMin;
-            this.yMax = yMax;
-        }
-
-    }    
-
+    }
 
     class Program
     {
-        // Create a border
-        static void MakeBorder(List <Pos> border, int xMin, int xMax, int yMin, int yMax)
-        {
-            for (int x = xMin; x <= xMax; x++)
-            {
-                border.Add(new Pos(x, yMin));
-                border.Add(new Pos(x, yMax));
-            }
-
-            for (int y = yMin+1; y < yMax; y++)
-            {
-                border.Add(new Pos(xMin, y));
-                border.Add(new Pos(xMax, y));
-            }
-
-        }
-
-        static void MakeWall(List<Pos> wall, int xMin, int xMax, int yMin, int yMax)
-        {
-            for (int x = xMin; x <= xMax; x++)
-            {
-                wall.Add(new Pos(x, yMin));
-                wall.Add(new Pos(x, yMax));
-            }
-            for (int y = yMin + 1; y < yMax; y++)
-            {
-                wall.Add(new Pos(xMin, y));
-                wall.Add(new Pos(xMax, y));
-            }
-        }
-
-
-        static void MakeMap(LandStt[,] map, List<Pos> border)
-        {
-            for (int x = 0; x < map.GetLength(0); x++)
-            {
-                for (int y = 0; y < map.GetLength(1); y++)
-                {
-                    map[x, y] = new LandStt();
-                }
-            }
-            foreach (var pos in border)
-            {
-                map[pos.x,pos.y].SetBorder();
-            }
-        }
-
-        static void ShowListChar(List<Pos> listChar, char show)
-        {
-            foreach (var pos in listChar)
-            {
-                Console.SetCursorPosition(pos.x, pos.y);
-                Console.Write(show);
-            }
-        }
-
-
-        static void ShowMap(LandStt[,] map, Symbol sym)
-        {
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Red;
-            
-
-
-        }
 
         static void Main(string[] args)
         {
-           
-            int xMin = 0;
-            int sizeX = 100;
-            int yMin = 0;
-            int sizeY = 20;
-
-            int yMax = yMin + sizeY + 1;
-            int xMax = xMin + sizeX + 1;
-
-            Symbol sym = new Symbol();      // Create default symbol
-            LandStt[,] map = new LandStt[xMax+1, yMax+1];
-            
-            List <Pos> border = new List<Pos>();
-            MakeBorder(border, xMin, xMax, yMin, yMax);
-            
-            List <Pos> wall = new List<Pos>();
-            MakeWall(wall, xMin+1, xMax-1, yMin+1, yMax+1);
+            Circle c = new Circle();
+            c.Radius = 10;
+            c.Color = "Green";
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("Radius {0:F2}",c.Radius);
+            Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
 
 
-            MakeMap(map, border);
-            Console.CursorVisible = true;
-
-            ShowMap(map, sym);
-            ShowListChar(border, sym.Border);
-            ShowListChar(wall, sym.Wall);
+            Cylinder cylinder = new Cylinder();
+            cylinder.Radius = c.Radius;
+            cylinder.Color = c.Color;
+            cylinder.height = 20;
 
 
-            Console.ReadLine();
+            Console.WriteLine("------------");
+            c.Radius = 100; c.Color = "Red";        // Test thu doi gia tri, nhung Cylinder khong thay doi
+			Console.WriteLine(c.ToString());
+			Console.WriteLine("Radius {0:F2}", c.Radius);
+			Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
 
-            //Console.SetCursorPosition(10, 10);  // Set the cursor position to the specified coordinates
-            //Console.ForegroundColor = ConsoleColor.Red; // Set the color of the text
-            //Console.BackgroundColor = ConsoleColor.Black; // Set the background color of the text
-            //Console.WriteLine("O");             // Write the specified string value
-            //Console.ReadLine(); // Read the next line of characters from the standard input stream
+            Console.WriteLine("------------");
+			Console.WriteLine(cylinder.ToString());
+            Console.WriteLine("Cylinder radius {0:F2} - height {1:F2}", cylinder.Radius, cylinder.height);
+            Console.WriteLine("Color is " + cylinder.Color);
+            Console.WriteLine("Volumne is {0:F2}",cylinder.Volumne());
 
-            //Console.CursorVisible = false;
-            //Console.Clear(); // Clear the console
+            Console.WriteLine("------------");
+            cylinder.Radius = 200;
+            cylinder.Color = "White";
+            cylinder.height = 900;
+			Console.WriteLine(c.ToString());
+			Console.WriteLine("Radius {0:F2}", c.Radius);
+			Console.WriteLine("Color is " + c.Color);
+			Console.WriteLine("Area is {0:F2}", c.Area());
 
-        }
+		}
     }
 
 
-
 }
+
+
+
